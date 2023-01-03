7a5b77f47dd27b54b90a6204f1e54e673db0a026
Frommflin <freakyfrommelin@outlook.com>
2023-01-03 11:54:31 +0100

    Add project files.

---
+++ DnD-CharacterCollection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DnD_CharacterCollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
